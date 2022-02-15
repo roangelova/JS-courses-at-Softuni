4c5297ff2a69801031528f7a83f34c36e09595c3
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-02-15 12:29:00 +0200

    Site functionality improved

--- JS Advanced- Exam Preparation/Exam_27June2021/01. Travel Agency_Ресурси/01. Travel Agency/solution.js
+++ JS Advanced- Exam Preparation/Exam_27June2021/01. Travel Agency_Ресурси/01. Travel Agency/solution.js
@@ -2,50 +2,59 @@ window.addEventListener('load', solution);
 
 function solution() {
 
+  let editButton = document.getElementById('editBTN');
+  let continueButton = document.getElementById('continueBTN');
   let submitBtn = document.getElementById('submitBTN');
+
   submitBtn.addEventListener('click', submitData);
 
   function submitData(e) {
-    e.preventDefault;
+    e.preventDefault();
     let infoPreview = document.getElementById('infoPreview');
     infoPreview.innerHTML = '';
 
-    let name = document.getElementById('fname').value;
-    let email = document.getElementById('email').value;
-    let phoneNumber = document.getElementById('phone').value;
-    let address = document.getElementById('address').value;
-    let postalCode = document.getElementById('code').value;
+    let name = document.getElementById('fname');
+    let email = document.getElementById('email');
+    let phoneNumber = document.getElementById('phone');
+    let address = document.getElementById('address');
+    let postalCode = document.getElementById('code');
 
-    let data = [name, email, phoneNumber, address, postalCode];
-
-    if (name.value == '' || email.value == '') {
+    if (name.value === '' || email.value === '') {
       return;
     }
-    copiedData = [...data];
-    //clear the input
-    for (const inputField of data) {
-      inputField.value = '';
-    }
-    submitBtn.disabled = true;
 
-    //get remaining buttons
-    let editButton = document.getElementById('editBTN');
-    let continueButton = document.getElementById('continueBTN');
+    let initialData = [name.value, email.value, phoneNumber.value, address.value, postalCode.value];
+
+
+    infoPreview.innerHTML = `<li>Full Name: ${name.value}</li>
+      <li>Email: ${email.value}</li>
+      <li>Phone Number: ${phoneNumber.value}</li>
+      <li>Address: ${address.value}</li>
+      <li>Postal Code: ${postalCode.value}</li>`;
+
+    submitBtn.disabled = true;
     editButton.disabled = false;
     continueButton.disabled = false;
 
-    
+    name.value = '';
+    email.value = '';
+    phoneNumber.value = '';
+    address.value = '';
+    postalCode.value = '';
 
-    editButton.addEventListener('click', displayData);
+    editButton.addEventListener('click', editData);
+    continueButton.addEventListener('click', finishReservation);
 
+    function editData(e) {
+      e.preventDefault();
 
-    function displayData(e) {
-      e.preventDefault;
-      infoPreview.innerHTML = `<li>Full Name: ${copiedData[0]}</li>
-      <li>Email: ${copiedData[1]}</li>
-      <li>Phone Number: ${copiedData[2]}</li>
-      <li>Address: ${copiedData[3]}</li>
-      <li>Postal Code: ${copiedData[4]}</li>`;
+      infoPreview.innerHTML = '';
+
+      name.value = initialData[0];
+      email.value = initialData[1];
+      phoneNumber.value = initialData[2];
+      address.value = initialData[3];
+      postalCode.value = initialData[4];
 
       editButton.disabled = true;
       continueButton.disabled = true;
@@ -53,19 +62,19 @@ function solution() {
 
       submitBtn.addEventListener('click', submitData);
 
-//continueButton.disabled = false;
+      //continueButton.disabled = false;
       continueButton.addEventListener('click', finishReservation);
 
+    }
 
-      function finishReservation(e) {
-        e.preventDefault;
-infoPreview.innerHTML = '';
-        let blockElement = document.getElementById('block');
-        let h3 = document.createElement('h3');
-        h3.innerText = "Thank you for your reservation!";
-        blockElement.innerHTML = '';
-        blockElement.appendChild(h3);
-      }
+    function finishReservation(e) {
+      e.preventDefault();
+      infoPreview.innerHTML = '';
+      let blockElement = document.getElementById('block');
+      let h3 = document.createElement('h3');
+      h3.innerText = "Thank you for your reservation!";
+      blockElement.innerHTML = '';
+      blockElement.appendChild(h3);
     }
   }
 }
