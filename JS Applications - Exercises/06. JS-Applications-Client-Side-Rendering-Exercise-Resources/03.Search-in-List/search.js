3ef32523d795a2b075cb4261b76ce61f37eaece8
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-03-15 09:07:50 +0200

    03.added

---
+++ JS Applications - Exercises/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
@@ -0,0 +1,37 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js';
+
+const searchTemplate = (towns, match) => html`
+<article>
+        <div id="towns">
+            <ul>
+                ${towns.map(t => itemTemplate(t, match))}
+            </ul>
+        </div>
+        <input type="text" id="searchText" />
+        <button @click =${search}>Search</button>
+        <div id="result">${countMatched(towns, match)}</div>
+    </article>
+`;
+
+const itemTemplate = (name, match) => html`
+<li class = ${(match && name.toLowerCase().includes(match.toLowerCase())) ? 'active' : ''}> ${name}</li>
+`;
+
+const main = document.body;
+
+function update(match = ''){
+   const result = searchTemplate(towns, match);
+   render(result, main);
+}
+update();
+
+function search() {
+const match = document.getElementById('searchText').value;
+update(match);
+}
+
+function countMatched(towns, match){
+   const matches = towns.filter(t => match && t.toLocaleLowerCase().includes(match.toLocaleLowerCase())).length;
+   return matches ? `${matches} matches found` : 'No matches found';
+}
