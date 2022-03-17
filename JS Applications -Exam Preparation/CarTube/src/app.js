2e3691790d77188407129bfc436440e782c6db16
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-03-17 16:11:47 +0200

    page register added

--- JS Applications -Exam Preparation/CarTube/src/app.js
+++ JS Applications -Exam Preparation/CarTube/src/app.js
@@ -4,12 +4,13 @@ import {navigationMiddleware} from './middlewares/navigationMiddleware.js';
 import {renderMiddleware} from  './middlewares/renderMiddleware.js';
 
 import {renderLogin} from './views/loginView.js';
+import { renderRegister } from './views/registerView.js';
 
 page(navigationMiddleware);
 page(renderMiddleware);
 
 page('/', () => console.log('loaded'));
 page('/login', renderLogin);
-page('/register', '');
+page('/register', renderRegister);
 
 page.start();
