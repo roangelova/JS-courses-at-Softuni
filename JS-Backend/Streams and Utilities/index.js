93d21740f87df812495fc3fcc9c8afa3d18413e2
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-09-14 16:06:47 +0300

    Delete JS-Backend directory

--- JS-Backend/Streams and Utilities/index.js
+++
@@ -1,8 +0,0 @@
-//we must first subscribe, in order to get the data
-require('./subscriber');
-const publish = require('./publisher');
-
-let index = 0;
-setInterval(() => {
-    publish(++index);
-}, 2000)
--- JS-Backend/Streams and Utilities/publisher.js
+++
@@ -1,8 +0,0 @@
-const observer = require('./observer');
-
-function publish(index) {
-    console.log('Publish called');
-    observer.emit('alert', `Publishing event ${index} times`);
-}
-
-module.exports = publish;
